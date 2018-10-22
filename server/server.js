068a848bb9b4db2b65e42d267171a1106b9c309e
Akshay Tenkale <tenkaleakshay94@gmail.com>
2018-10-22 13:07:05 -0400

    create and receive message

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'John cena',
+    text: 'Hey. What is going on.',
+    createdAt: 123
+  });
+
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
