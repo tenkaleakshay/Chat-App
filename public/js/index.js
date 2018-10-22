74e2db37ef4780632dca8b6f5457d480af98e743
Akshay Tenkale <tenkaleakshay94@gmail.com>
2018-10-22 16:32:42 -0400

    Added forms and Jquery

--- public/js/index.js
+++ public/js/index.js
@@ -12,4 +12,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(message){
   console.log('newMessage', message);
+  var list = jQuery('<li></li>');
+  list.text(`${message.from}:${message.text}`);
+
+  jQuery('#messages').append(list);
+});
+
+// socket.emit('createMessage', {
+//   from:'Akshay',
+//   text: 'Bullshit'
+// }, function(data){
+//   console.log('got it:', data);
+// });
+
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function(){
+
+  })
 })
